f6929f7daf7ed85f9245d1dba326d397edb7a970
Samim Aftab <samimaftab@samimmac.local>
2026-03-28 14:51:21 +0530

    Polished UI: added sidebar, metrics and progress bar

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import streamlit as st 
 from textblob import TextBlob
 
-sentiment = 0.0
+
 
 st.set_page_config(page_title="Sentiment Analyzer", page_icon="☕️")
+
+st.sidebar.title("App Info")
+st.sidebar.info("This minimalist web app uses Natural Language Processing to instantly calculate the emotional tone of your text")
+st.sidebar.caption("Built with Python & Streamlit")
+
 st.title("Sentiment Analysis App")
 st.divider()
 st.write("Enter a sentence to analyze its emotional tone.")
@@ -14,11 +19,15 @@ if st.button("Analyze Sentiment"):
         blob = TextBlob(user_input)
         sentiment = blob.sentiment.polarity 
 
-    if sentiment > 0:
-        st.success("Sentiment: Positive")
-    elif sentiment < 0:
-        st.error("Sentiment: Negative")
-    else:
-        st.info("Sentiment: Neutral")
+        if sentiment > 0:
+            st.success("Sentiment: Positive")
+        elif sentiment < 0:
+            st.error("Sentiment: Negative")
+        else:
+            st.info("Sentiment: Neutral")
 
-    st.write("Polarity Score: ", sentiment)
+        st.metric(label="Polarity Score", value=round(sentiment,2))
+        normalised_score = (sentiment+1)/2
+        st.progress(normalised_score)
+    else:
+        st.warning("Please enter some text to analyse!")
